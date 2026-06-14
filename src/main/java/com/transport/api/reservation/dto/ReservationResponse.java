622d9cc45cd8b39a04b21face34ab858d156f697
JeanLeon KABOBI <jeanleon.kabobi@gmail.com>
2026-06-14 14:16:57 +0000

    Modification de ResvationResponse

--- src/main/java/com/transport/api/reservation/dto/ReservationResponse.java
+++ src/main/java/com/transport/api/reservation/dto/ReservationResponse.java
@@ -1,4 +1,26 @@
 package com.transport.api.reservation.dto;
 
+import com.transport.api.reservation.enums.StatutReservation;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
 public class ReservationResponse {
+    private Long id;
+    private UUID reservationCode;
+    private Long trajetId;
+    private String departureCity;
+    private String arrivalCity;
+    private LocalDateTime departureTime;
+    private List<String> sieges;
+    private Double totalPrice;
+    private StatutReservation status;
+    private LocalDateTime expiresAt;
+    private String passengerName;
+    private String passengerPhone;
 }
